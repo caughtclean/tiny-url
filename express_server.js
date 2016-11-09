92446e3914b0be61342be0d83a40d2367056c0cf
Nathan Eyerley <eyerleyn@gmail.com>
2016-11-09 02:36:49 +0000

    Added new_url page, and random 6 digit key generator

--- express_server.js
+++ express_server.js
@@ -1,13 +1,18 @@
 var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080; // default port 8080
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs")
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
+
+
 };
+
 
 app.get("/", (req, res) => {
   res.end("Hello!");
@@ -20,6 +25,15 @@ app.get("/urls.json", (req, res) => {
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // debug statement to see POST parameters
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/urls/:id", (req, res) => {
@@ -36,3 +50,13 @@ app.get("/hello", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+function generateRandomString() {
+    let possible = "ABCDEFGHIJKLMOPQRSTUVWXYZ123456789abcdefghijklmnopqrstuvwxyz"
+    let random = ""
+    for (let i = 0; i < 6; i += 1) {
+      random += possible.charAt(Math.floor(Math.random() * possible.length));
+    };
+    return random
+};
+    console.log(generateRandomString())
